b3788f15f54b8af5cce57b82ef208999bb404762
Mhamad  Farhat <mfarhat26@gmail.com>
2024-07-10 02:38:25 +0300

    Frontend env and common folder changed

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import router from "./routes/index.js";
 import cookieParser from "cookie-parser";
+
 // Initialize dotenv to load environment variables from .env file
 dotenv.config();
 
@@ -12,22 +13,23 @@ const app = express();
 // Middleware to enable CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Example fallback if FRONTEND_URL is not defined
     credentials: true,
   })
 );
-app.use(express.json({ limit: '50mb' }));
-app.use(cookieParser());
-app.use("/api", router);
 
 // Middleware to parse JSON requests
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser());
+
+// Routes
+app.use("/api", router);
 
 const PORT = process.env.PORT || 8080;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("connected to DB");
+    console.log("Connected to DB");
     console.log(`Server is running on port ${PORT}`);
   });
 });
