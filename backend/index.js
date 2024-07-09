30d13b8b2e3f49d7455dbd8b00449fa5f569edf7
Mhamad  Farhat <mfarhat26@gmail.com>
2024-07-10 02:41:38 +0300

    Frontend env and common folder changed

--- backend/index.js
+++ backend/index.js
@@ -5,24 +5,23 @@ import connectDB from "./config/db.js";
 import router from "./routes/index.js";
 import cookieParser from "cookie-parser";
 
-// Initialize dotenv to load environment variables from .env file
 dotenv.config();
 
 const app = express();
 
-// Middleware to enable CORS
+// Enable CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Example fallback if FRONTEND_URL is not defined
+    origin: process.env.FRONTEND_URL || "https://sports-ecommerce-imad.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-// Middleware to parse JSON requests
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.use(cookieParser());
 
-// Routes
 app.use("/api", router);
 
 const PORT = process.env.PORT || 8080;
