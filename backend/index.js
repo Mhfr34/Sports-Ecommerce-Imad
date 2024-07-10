c8d171e476029011ef3ad75724d4dc64cd03a30c
Mhamad  Farhat <mfarhat26@gmail.com>
2024-07-10 03:29:19 +0300

    Frontend env and common folder changed

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,15 @@ app.use(cookieParser());
 
 // CORS middleware function
 const allowCors = (fn) => async (req, res) => {
+  const allowedOrigins = ['https://sports-ecommerce-imad-nm1u-a3x5oikle-mhamad-farhats-projects.vercel.app'];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*'); // or req.headers.origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
