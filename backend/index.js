c3cbfbae472e9009f36b742713b0d86c3e132c2e
Mhamad  Farhat <mfarhat26@gmail.com>
2024-07-10 03:11:25 +0300

    Frontend env and common folder changed

--- backend/index.js
+++ backend/index.js
@@ -4,30 +4,28 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import router from "./routes/index.js";
 import cookieParser from "cookie-parser";
-// Initialize dotenv to load environment variables from .env file
-dotenv.config();
+
+dotenv.config(); // Initialize dotenv to load environment variables from .env file
 
 const app = express();
 
-// Middleware to enable CORS
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
-app.use(express.json({ limit: '50mb' }));
-app.use(cookieParser());
-app.use("/api", router);
+// CORS middleware configuration
+app.use(cors({
+    origin: process.env.FRONTEND_URL, // Allow requests from this origin
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    allowedHeaders: ["Authorization", "Content-Type"] // Specify allowed headers
+}));
 
-// Middleware to parse JSON requests
-app.use(express.json());
+app.use(express.json()); // Parse JSON bodies
+app.use(cookieParser()); // Parse cookies
 
-const PORT = process.env.PORT || 8080;
+app.use("/api", router); // Route requests to /api to the router
+
+const PORT = process.env.PORT || 8080; // Use the port from environment variables or default to 8080
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("connected to DB");
-    console.log(`Server is running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log("Connected to DB");
+        console.log("Server is running on port " + PORT);
+    });
 });
