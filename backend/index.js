71c9bd304881ba2c8cf3e1ce819cf745bcb5bf07
Mhamad  Farhat <mfarhat26@gmail.com>
2024-07-10 15:19:15 +0300

    Same site none in logout and login

--- backend/index.js
+++ backend/index.js
@@ -16,28 +16,28 @@ app.use(cookieParser());
 
 // CORS middleware function
 const allowCors = (fn) => async (req, res) => {
-  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+    const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
 
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
 
-  // Handle preflight requests (OPTIONS)
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
+    // Handle preflight requests (OPTIONS)
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
 
-  // Call the actual request handler function
-  return await fn(req, res);
+    // Call the actual request handler function
+    return await fn(req, res);
 };
 
 // Apply CORS middleware to all routes under /api
@@ -46,8 +46,8 @@ app.use("/api", allowCors(router));
 // Connect to database
 const PORT = process.env.PORT || 8080;
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Connected to DB");
-    console.log(`Server is running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log("Connected to DB");
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
