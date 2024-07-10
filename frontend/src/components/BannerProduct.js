60de7845b8b8a116f50b4f525d3767ea112823c6
Mhamad  Farhat <mfarhat26@gmail.com>
2024-07-10 16:08:44 +0300

    Update filtration

--- frontend/src/components/BannerProduct.js
+++ frontend/src/components/BannerProduct.js
@@ -7,11 +7,11 @@ import image3 from "../assest/banner/img11.jpg";
 import image4 from "../assest/banner/img9.jpg";
 import image5 from "../assest/banner/img8.png";
 
-import image1Mobile from "../assest/banner/img1_mobile.jpg";
-import image2Mobile from "../assest/banner/img2_mobile.webp";
-import image3Mobile from "../assest/banner/img3_mobile.jpg";
-import image4Mobile from "../assest/banner/img4_mobile.jpg";
-import image5Mobile from "../assest/banner/img5_mobile.png";
+import image1Mobile from "../assest/banner/adidas.jpg";
+import image2Mobile from "../assest/banner/samsung.jpg";
+import image3Mobile from "../assest/banner/mouse.jpg";
+import image4Mobile from "../assest/banner/shoes.jpg";
+
 
 const BannerProduct = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -21,7 +21,7 @@ const BannerProduct = () => {
     image2Mobile,
     image3Mobile,
     image4Mobile,
-    image5Mobile,
+
   ];
   const nextImage = () => {
     if (desktopImages.length - 1 > currentImage) {
